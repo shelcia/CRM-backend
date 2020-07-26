f95e039e83fad7187165280574c497d8088974fa
Shelcia abi <shelciaabi@Shelcias-MacBook-Air-2320.local>
2020-07-27 01:01:10 +0530

    update

--- routes/post.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const verify = require("./verifyToken");
-
-router.get("/", verify, (req, res) => {
-  //   res.json({ post: { title: "my first post ", description: "you know it" } });
-  res.send(req.user);
-});
-
-module.exports = router;
